45177a57c2c2e331403e19c8d6ced5dc1ee596e8
vraj3724 <vrajpatel3724@gmail.com>
2025-07-18 21:42:27 +0100

    Fixed root directory

--- app.js
+++ app.js
@@ -69,9 +69,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("The root is working");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
