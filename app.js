6f1a308ab9180bb0cbdb712d8d15fd3954139441
vraj3724 <vrajpatel3724@gmail.com>
2025-07-18 21:18:45 +0100

    Removed Unnecessary codes

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user.js');
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 const listingRouter = require('./routes/listing.js');
@@ -48,7 +47,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret : "mysupersecretcode",
+    secret : process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -60,7 +59,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
